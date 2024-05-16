6efa6814a20e44e3ae0ef4c9ecfa3f04f0c6febc
wrlakshan <rashmika.lakshan@ebuilder.com>
2024-05-16 12:45:13 +0530

    add s3 bucket

--- lib/cdk-s3-stack.ts
+++ lib/cdk-s3-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { aws_s3 as s3 } from 'aws-cdk-lib';
 
 export class CdkS3Stack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -12,5 +13,11 @@ export class CdkS3Stack extends cdk.Stack {
     // const queue = new sqs.Queue(this, 'CdkS3Queue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
+
+    new s3.Bucket(this, 'MyFirstBucket', {
+      versioned: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true
+    });
   }
 }
